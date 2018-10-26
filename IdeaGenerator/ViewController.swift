bfe623fb6ea78e4f7269cffa18bada4ff6631c3c
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-26 00:03:30 -0700

    first commit

--- IdeaGenerator/ViewController.swift
+++ IdeaGenerator/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var startupLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
+    let startups = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"];
+    
+    let targets = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func generateIdea() {
+//        generating startups
+        let maxStartupIndex = UInt32(startups.count)
+        let randomStartupIndex = Int(arc4random_uniform(maxStartupIndex))
+        startupLabel.text = startups[randomStartupIndex]
+        
+//         generating target for startups
+        let maxTargetIndex = UInt32(targets.count)
+        let randomTargetIndex = Int(arc4random_uniform(maxTargetIndex))
+        targetLabel.text = targets[randomTargetIndex]
+    }
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateIdea()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        
+        generateIdea()
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,11 @@
+# Idea Generator
+
+**Idea Generator** is an IOS app that generates funny startup ideas
+
+
+## User Stories
+* [X] Users can see startups and potential targets on the screen
+* [X] Users can click "Shake it!" button to generate companies and their customers
+* [X] Users can shake their iPhones to generate companies and their customers
+
+
